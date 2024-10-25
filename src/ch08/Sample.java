72ef98575c624ed1ad21a5ddde65ba8fdf1a5f9e
rok <rok@gmail.com>
2024-10-25 11:27:05 +0900

    샘플파일 추가

---
+++ src/ch08/Sample.java
@@ -0,0 +1,12 @@
+package ch08;
+
+public class Sample {
+    static int[] gugu(int dan){
+        System.out.println(dan);
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] result=gugu(2);
+    }
+}
